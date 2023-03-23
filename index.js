e0048566a2970c6ca7c09e035d7d4ec2c032f408
teknoreceh <danaputrayuda@gmail.com>
2023-03-24 00:58:13 +0700

    coba looping pakai for

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const myName = "Dana Putra Yuda";
 function getName(){
-    console.log(myName);
+    for (let i = 0; i < 10; i++) {
+        console.log('nama saya adalah ', myName )
+    }
 };
 
 getName();
